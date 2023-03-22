f78b10d1331ff4b0bb568d243a5535d8f2442ce5
sajim-khan <shossain1427@gmail.com>
2023-03-23 02:20:24 +0600

    added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,6 +1,7 @@
 //2nd (data send for showing in UI)
 import React, { useEffect, useState } from "react";
 import Country from "./Country/Country";
+import './Countries.css'
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
@@ -13,13 +14,11 @@ const Countries = () => {
   return (
     <div>
       <h1>Hello from countries: {countries.length}</h1>
-      {/* {countries.map((country) => console.log(country))} */}
-
-      {countries.map(country => 
-      <Country 
-      country = {country}
-      ></Country>  
-      )}
+      <div className="countries-container">
+        {countries.map((country) => (
+          <Country country={country} key={country.cca3}></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Countries/Country/Country.js
+++ src/components/Countries/Country/Country.js
@@ -3,17 +3,15 @@ import React from "react";
 import "./Country.css";
 
 const Country = (props) => {
-  console.log(props);
+  // console.log(props.country.languages);
   const { name, region, population, flags, capital, languages } = props.country;
   return (
     <div className="country">
       <h1>Country name: {name.common}</h1>
-      {/* <h3>Region: {region}</h3>
-      <h4>Population: {population}</h4>
-      <h5>Capital: {capital}</h5>
-      <h5>Language: {languages}</h5> */}
-
       <img src={flags.png} alt="" />
+      <h3>Region: {region}</h3>
+      <h4>Population: {population}</h4>
+      <h4>Capital: {capital}</h4>
     </div>
   );
 };
